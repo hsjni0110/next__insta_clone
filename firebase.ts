9643bb15dcdebfb92a7118de6f1e38ac6b4e39f1
hsjni0110 <92289935+hsjni0110@users.noreply.github.com>
2022-10-31 15:33:13 +0900

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -9,13 +9,7 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDvll9tKbnaBSqWB2ZOts_kvILIR-IMpZs",
-  authDomain: "insta-clone-c33e6.firebaseapp.com",
-  projectId: "insta-clone-c33e6",
-  storageBucket: "insta-clone-c33e6.appspot.com",
-  messagingSenderId: "328876975159",
-  appId: "1:328876975159:web:9d267bf1b35a7e1759cbb2",
-  measurementId: "G-S7J5434FDH"
+
 };
 
 // Initialize Firebase
